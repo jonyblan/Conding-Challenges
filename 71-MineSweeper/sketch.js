a3b1d8a2861c2d751a95a3d156e24927c04aff3c
Jony <102042390+jonyblan@users.noreply.github.com>
2024-02-25 18:37:19 -0300

    Made the bot
    Its buggy, the code is ugly but i wont fix it now. The bot code has bugs and its just not legible, so I'll come back to this in the future and just restart with the bot

--- 71-MineSweeper/sketch.js
+++ 71-MineSweeper/sketch.js
@@ -1,6 +1,6 @@
 let rows = 10, cols = 10;
 
-let cantMines = 5;
+let cantMines = 20;
 
 let cellWidth, cellHeight;
 
@@ -75,6 +75,64 @@ function addMine(i, j){
 	return 1;
 }
 
+function checkSpot(i, j){
+	if(!grid[i][j].revealed){
+		grid[i][j].reveal();
+		for(let a = -1; a <= 1; a++){
+			for(let b = -1; b <= 1; b++){
+				if(validIndex(i + a, j + b, rows, cols)){
+					grid[i + a][j + b].hiddenNeighbours--;
+				}
+			}
+		}
+		grid[i][j].hiddenNeighbours++;
+		newMove = true;
+	}
+}
+
+function putFlag(i, j){
+	if(!grid[i][j].revealed){
+		if(addMine(i, j)){
+			grid[i][j].revealed = true;
+			for(let a = -1; a <= 1; a++){
+				for(let b = -1; b <= 1; b++){
+					if(validIndex(i + a, j + b, rows, cols)){
+						grid[i + a][j + b].hiddenNeighbours--;
+					}
+				}
+			}
+			grid[i][j].hiddenNeighbours++;
+			newMove = true;
+		}
+	}
+}
+
+function sameAroundHidden(i, j){
+	for(let a = -1; a <= 1; a++){
+		for(let b = -1; b <= 1; b++){
+			if(validIndex(i + a, j + b, rows, cols)){
+				putFlag(i + a, j + b);
+			}
+		}
+	}
+}
+
+function getCantNeighboursHidden(i, j){
+	let cantNeighboursHidden = grid[i][j].cantNeighbours;
+	for(let a = -1; a <= 1; a++){
+		for(let b = -1; b <= 1; b++){
+			if(validIndex(i + a, j + b, rows, cols)){
+				if(grid[i + a][j + b].revealed == true){
+					if(grid[i + a][j + b].mine == true){
+						cantNeighboursHidden--;
+					}
+				}
+			}
+		}
+	}
+	return cantNeighboursHidden;
+}
+
 function computerMove(i, j){
 	if(lastMove.length == 0){
 		let n = Math.floor(random(rows)), m = Math.floor(random(cols));
@@ -86,48 +144,27 @@ function computerMove(i, j){
 	if(!grid[i][j].revealed){
 		return ;
 	}
-	// same mines arround as hidden cells arround
-	if(grid[i][j].cantNeighbours == grid[i][j].hiddenNeighbours){
-		for(let a = -1; a <= 1; a++){
-			for(let b = -1; b <= 1; b++){
-				if(validIndex(i + a, j + b, rows, cols)){
-					if(!grid[i + a][j + b].revealed){
-						if(addMine(i + a, j + b)){
-							grid[i + a][j + b].revealed = true;
-							newMove = true;
-						}
-					}
-				}
-			}
-		}
+	if(grid[i][j].hiddenNeighbours == 0){
+		return ;
 	}
-	else{
-		// 0 mines arround
-		let mineCount = 0;
+	if(grid[i][j].mine){
+		return ;
+	}
+
+	// same mines arround as hidden cells arround
+	let cantNeighboursHidden = getCantNeighboursHidden(i, j);
+	if(cantNeighboursHidden == grid[i][j].hiddenNeighbours){
+		sameAroundHidden(i, j);
+	}
+
+	// same non mines arround as hidden cells arround
+	if(cantNeighboursHidden == 0 && grid[i][j].hiddenNeighbours != 0){
 		for(let a = -1; a <= 1; a++){
 			for(let b = -1; b <= 1; b++){
 				if(validIndex(i + a, j + b, rows, cols)){
-					if(grid[i + a][j + b].revealed){
-						if(grid[i + a][j + b].mine){
-							mineCount++;
-						}
-					}
+					checkSpot(i + a, j + b);
 				}
 			}
-		}
-		if(mineCount == 0 || grid[i][j].hiddenNeighbours == 0){
-			return ;
-		}
-		if(grid[i][j].cantNeighbours == mineCount){
-			//console.log(i + " - " + j + " - " + mineCount);
-			for(let a = -1; a <= 1; a++){
-				for(let b = -1; b <= 1; b++){
-					if(validIndex(i + a, j + b, rows, cols)){
-						grid[i + a][j + b].reveal(grid, rows, cols);
-					}
-				}
-			}
-			newMove = true;
 		}
 	}
 }
@@ -135,17 +172,33 @@ function computerMove(i, j){
 function draw(){
 	background(0);
 	newMove = false;
+	let shownMines = 0;
 	for(let i = 0; i < rows; i++){
 		for(let j = 0; j < cols; j++){
 			computerMove(i, j);
 			grid[i][j].show(i, j, cellWidth, cellHeight);
+			if(grid[i][j].mine && grid[i][j].revealed){
+				shownMines++;
+			}
 		}
 	}
+	if(shownMines > mines.length){
+		noLoop();
+	}
 	for(let i = 0; i < mines.length; i++){
+		if(grid[mines[i][0]][mines[i][1]].mine == false){
+			console.log("Wrong mine at: [" + mines[i][0] + " , " + mines[i][1] + "]");
+			noLoop();
+		}
 		fill(0, 0, 255);
 		ellipse(mines[i][0]*cellWidth + cellWidth/2, mines[i][1]*cellHeight + cellHeight / 2, cellWidth / 2, cellHeight / 2);
 	}
-	if(!newMove){
+	if(newMove == false){
+		let n = Math.floor(random(rows)), m = Math.floor(random(cols));
+		grid[n][m].reveal(grid, rows, cols);
+	}
+	if(mines.length >= cantMines){
 		noLoop();
+		console.log(mines.length);
 	}
 }
